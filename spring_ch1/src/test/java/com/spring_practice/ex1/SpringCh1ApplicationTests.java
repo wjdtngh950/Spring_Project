8c124ce70eb454f89031daf13a5d231c460191ca
wjdtngh950 <wjdtngh950@naver.com>
2021-11-01 18:26:47 +0900

    ch1_start

---
+++ spring_ch1/src/test/java/com/spring_practice/ex1/SpringCh1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_practice.ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCh1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
