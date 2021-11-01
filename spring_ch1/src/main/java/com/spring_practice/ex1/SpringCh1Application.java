d14c613f528770a338f91609edc89b18c6317258
wjdtngh950 <wjdtngh950@naver.com>
2021-11-01 19:15:20 +0900

    Update SpringCh1Application.java

--- spring_ch1/src/main/java/com/spring_practice/ex1/SpringCh1Application.java
+++ spring_ch1/src/main/java/com/spring_practice/ex1/SpringCh1Application.java
@@ -3,11 +3,12 @@ package com.spring_practice.ex1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringCh1Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringCh1Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCh1Application.class, args);
+	}
 
 }
