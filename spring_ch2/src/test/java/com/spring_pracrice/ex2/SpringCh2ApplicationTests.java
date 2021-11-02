8509b27875461152bc8dc7ea6fbdae2e94d612ca
wjdtngh950 <wjdtngh950@naver.com>
2021-11-02 21:56:51 +0900

    ch2

---
+++ spring_ch2/src/test/java/com/spring_pracrice/ex2/SpringCh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_pracrice.ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCh2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
