e9d618ba4cd282218a13727e6baf95035d882dfb
wjdtngh950 <wjdtngh950@naver.com>
2021-11-03 16:31:11 +0900

    test_ch2

---
+++ spring_test/src/test/java/com/example/test_ex2/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
