9f9a2027923aa1e1e07d5c285c6fa441173bbe3d
wjdtngh950 <wjdtngh950@naver.com>
2021-11-05 19:32:00 +0900

    Create MemoRepositoryTests.java

---
+++ spring_ch2/src/test/java/com/spring_pracrice/ex2/repository/MemoRepositoryTests.java
@@ -0,0 +1,11 @@
+package com.spring_pracrice.ex2.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MemoRepositoryTests {
+    @Autowired
+    MemoRepository memoRepository;
+
+}
