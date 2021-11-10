58ff93c4019f6fb5d0ad1d41fb366b69774219b6
wjdtngh950 <wjdtngh950@naver.com>
2021-11-10 23:00:18 +0900

    Update MemoRepositoryTests.java

--- spring_test/src/test/java/com/example/test_ex2/repository/MemoRepositoryTests.java
+++ spring_test/src/test/java/com/example/test_ex2/repository/MemoRepositoryTests.java
@@ -1,8 +1,12 @@
 package com.example.test_ex2.repository;
 
+import com.example.test_ex2.entity.Memo;
+import org.hibernate.sql.Insert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
 public class MemoRepositoryTests {
@@ -12,5 +16,14 @@ public class MemoRepositoryTests {
     @Test
     public void testclass(){
         System.out.println(memoRepository.getClass().getName());
+    }
+
+    @Test
+    public void testInsertDummies(){
+
+        IntStream.rangeClosed(1, 100).forEach(i->{
+            Memo memo =Memo.builder().memoText("Sample..."+i).build();
+            memoRepository.save(memo);
+        });
     }
 }
