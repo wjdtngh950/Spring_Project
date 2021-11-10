baf73395002907b24cc43720466d1d0c53b3205f
wjdtngh950 <wjdtngh950@naver.com>
2021-11-10 22:51:43 +0900

    Update Memo.java

--- spring_test/src/main/java/com/example/test_ex2/entity/Memo.java
+++ spring_test/src/main/java/com/example/test_ex2/entity/Memo.java
@@ -1,20 +1,24 @@
 package com.example.test_ex2.entity;
 
+
 import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
-@Table
-@ToString
-@Getter
 @Builder
-@AllArgsConstructor
+@Table(name = "tbl_memo")
+@ToString
 @NoArgsConstructor
+@AllArgsConstructor
+@Getter
+
+
 public class Memo {
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long memo;
-   @Column(length = 200, nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+    @Column(length = 200, nullable = false)
     private String memoText;
+
 }
