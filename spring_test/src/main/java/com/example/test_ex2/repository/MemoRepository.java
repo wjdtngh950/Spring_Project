29be1cbd1319b8e10d4797796557732b58dccd94
wjdtngh950 <wjdtngh950@naver.com>
2021-11-10 22:56:28 +0900

    Update MemoRepository.java

--- spring_test/src/main/java/com/example/test_ex2/repository/MemoRepository.java
+++ spring_test/src/main/java/com/example/test_ex2/repository/MemoRepository.java
@@ -5,5 +5,5 @@ import com.example.test_ex2.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-    
+
 }
