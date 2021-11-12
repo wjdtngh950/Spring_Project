1b59fc33ee52746cddd9fa5fb6f461e015d766ce
wjdtngh950 <wjdtngh950@naver.com>
2021-11-12 21:22:15 +0900

    ch3

---
+++ spring_ch3/src/main/java/com/example/ex3/SpringCh3Application.java
@@ -0,0 +1,13 @@
+package com.example.ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCh3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCh3Application.class, args);
+    }
+
+}
---
+++ spring_ch3/src/test/java/com/example/ex3/SpringCh3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCh3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
