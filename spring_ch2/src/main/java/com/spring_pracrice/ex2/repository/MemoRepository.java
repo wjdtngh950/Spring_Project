8c65cdbcbda59adf5092d8614d3d274b7e869d35
wjdtngh950 <wjdtngh950@naver.com>
2021-11-12 20:51:55 +0900

    Update MemoRepository.java

--- spring_ch2/src/main/java/com/spring_pracrice/ex2/repository/MemoRepository.java
+++ spring_ch2/src/main/java/com/spring_pracrice/ex2/repository/MemoRepository.java
@@ -3,6 +3,10 @@ package com.spring_pracrice.ex2.repository;
 import com.spring_pracrice.ex2.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long form, Long to);
+
 
 }
