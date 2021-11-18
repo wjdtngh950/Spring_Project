d289235273fe80ca001c0d53136156e1b10c2859
wjdtngh950 <wjdtngh950@naver.com>
2021-11-18 22:11:07 +0900

    Update MemoRepositoryTests.java

--- spring_test/src/test/java/com/example/test_ex2/repository/MemoRepositoryTests.java
+++ spring_test/src/test/java/com/example/test_ex2/repository/MemoRepositoryTests.java
@@ -75,7 +75,10 @@ public class MemoRepositoryTests {
         System.out.println("Page Number: " + result.getNumber());
         System.out.println("Page Size: " + result.getSize());
         System.out.println("has next page?: " + result.hasNext());
-        System.out.
+        System.out.println("first page?: " + result.isFirst());
+        System.out.println("--------------------------------------------");
+        for(Memo memo : result.getContent()){
+            System.out.println(memo);
         }
     }
 }
