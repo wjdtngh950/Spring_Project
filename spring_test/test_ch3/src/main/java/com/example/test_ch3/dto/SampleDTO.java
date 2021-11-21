e745d1890670f3a9d765c961074792747111cd13
wjdtngh950 <wjdtngh950@naver.com>
2021-11-21 21:31:00 +0900

    Create SampleDTO.java

---
+++ spring_test/test_ch3/src/main/java/com/example/test_ch3/dto/SampleDTO.java
@@ -0,0 +1,18 @@
+package com.example.test_ch3.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+    private Long sno;
+
+    private String first;
+
+    private String last;
+
+    private LocalDateTime regTime;
+}
