8b8cc55775f9094b9a5db18aaf85e40828fde622
wjdtngh950 <wjdtngh950@naver.com>
2021-11-21 20:33:27 +0900

    test_ch3

---
+++ spring_test/test_ch3/src/main/java/com/example/test_ch3/TestCh3Application.java
@@ -0,0 +1,13 @@
+package com.example.test_ch3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCh3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCh3Application.class, args);
+    }
+
+}
---
+++ spring_test/test_ch3/src/test/java/com/example/test_ch3/TestCh3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_ch3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCh3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
