a52628c731650da61d025ffa4ba7ce16b0d0f1b5
wjdtngh950 <wjdtngh950@naver.com>
2021-11-23 00:18:23 +0900

    Update SampleController.java

--- spring_ch3/src/main/java/com/example/ex3/controller/SampleController.java
+++ spring_ch3/src/main/java/com/example/ex3/controller/SampleController.java
@@ -1,8 +1,15 @@
 package com.example.ex3.controller;
 
+import com.example.ex3.dto.SampleDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/sample")
@@ -36,5 +43,19 @@ public class SampleController {
 
         // 지울때
         //글 삭제할 때
+    }
+
+    @GetMapping({"/ex2"})
+    public void exModel(Model model){
+        List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i->
+        {SampleDTO dto = SampleDTO.builder()
+                .sno(i)
+                .first("First.."+i)
+                .last("Last.."+i)
+                .regTime(LocalDateTime.now())
+                .build();
+            return dto;
+        }).collect(Collectors.toList());
+        model.addAttribute("list", list);
     }
 }
