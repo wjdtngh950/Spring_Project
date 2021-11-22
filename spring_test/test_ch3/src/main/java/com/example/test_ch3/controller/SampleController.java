b69a483692d747bd89b2c1784f7fb0d8788f9ae6
wjdtngh950 <wjdtngh950@naver.com>
2021-11-22 17:35:16 +0900

    Update SampleController.java

--- spring_test/test_ch3/src/main/java/com/example/test_ch3/controller/SampleController.java
+++ spring_test/test_ch3/src/main/java/com/example/test_ch3/controller/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,7 +17,7 @@ import java.util.stream.IntStream;
 @RequestMapping("/sample")
 @Log4j2
 
-public class SampleController {
+public class SampleController<redirectAttributes> {
     @GetMapping("/ex1")
     public void ex1(){
         log.info("ex1..................");
@@ -34,6 +35,26 @@ public class SampleController {
             return dto;
         }).collect(Collectors.toList());
         model.addAttribute("list", list);
+    }
+    @GetMapping({"/exInline"})
+            public String exInline(RedirectAttributes redirectAttributes) {
+        log.info("exInline..........");
+
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("First...100")
+                .last("Last ... 100")
+                .regTime(LocalDateTime.now())
+                .build();
+        redirectAttributes.addFlashAttribute("result", "success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+
+        return "redirect:/sample/ex3";
+
+    }
+    @GetMapping("/ex3")
+    public void ex3(){
+        log.info("ex3");
     }
 
 }
