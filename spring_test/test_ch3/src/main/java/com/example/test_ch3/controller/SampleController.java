384b5245c49504d664035f1320cb48b123a67205
wjdtngh950 <wjdtngh950@naver.com>
2021-11-22 18:36:28 +0900

    Update SampleController.java

--- spring_test/test_ch3/src/main/java/com/example/test_ch3/controller/SampleController.java
+++ spring_test/test_ch3/src/main/java/com/example/test_ch3/controller/SampleController.java
@@ -23,7 +23,7 @@ public class SampleController<redirectAttributes> {
         log.info("ex1..................");
     }
 
-    @GetMapping({"/ex2"})
+    @GetMapping({"/ex2", "/exLink"})
     public void exModel(Model model){
         List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i->{
             SampleDTO dto = SampleDTO.builder()
