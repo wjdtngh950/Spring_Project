9eda88bb8784bca9d7fb11439bd168cd39b4a3ef
wjdtngh950 <wjdtngh950@naver.com>
2021-11-24 21:40:31 +0900

    test_ch4

---
+++ spring_test/test_ch4/src/main/java/com/example/test_ch4/TestCh4Application.java
@@ -0,0 +1,13 @@
+package com.example.test_ch4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCh4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCh4Application.class, args);
+    }
+
+}
---
+++ spring_test/test_ch4/src/test/java/com/example/test_ch4/TestCh4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_ch4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCh4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
