486d74e81bce82a72e512559f5f18a15935a8561
wjdtngh950 <wjdtngh950@naver.com>
2021-11-26 18:59:13 +0900

    Update TestCh4Application.java

--- spring_test/test_ch4/src/main/java/com/example/test_ch4/TestCh4Application.java
+++ spring_test/test_ch4/src/main/java/com/example/test_ch4/TestCh4Application.java
@@ -3,7 +3,7 @@ package com.example.test_ch4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplicatio
 public class TestCh4Application {
 
     public static void main(String[] args) {
