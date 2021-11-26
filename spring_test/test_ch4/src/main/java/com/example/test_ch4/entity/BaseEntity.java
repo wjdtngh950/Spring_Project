50d824e444cf4a98eaf7c2823930ed69f4c84776
wjdtngh950 <wjdtngh950@naver.com>
2021-11-26 18:36:35 +0900

    Update BaseEntity.java

--- spring_test/test_ch4/src/main/java/com/example/test_ch4/entity/BaseEntity.java
+++ spring_test/test_ch4/src/main/java/com/example/test_ch4/entity/BaseEntity.java
@@ -2,10 +2,13 @@ package com.example.test_ch4.entity;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(value = {AuditingEntityListener.class})
@@ -14,4 +17,10 @@ import javax.persistence.MappedSuperclass;
 public class BaseEntity {
 
     @CreatedDate
+    @Column(name = "regdate", updatable =false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(name = "moddate")
+    private LocalDateTime moDate;
 }
