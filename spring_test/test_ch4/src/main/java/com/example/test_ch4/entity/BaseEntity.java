91bebbc52180a816c8a6e3a85be4ba3ead8fd120
wjdtngh950 <wjdtngh950@naver.com>
2021-11-26 18:34:46 +0900

    Update BaseEntity.java

--- spring_test/test_ch4/src/main/java/com/example/test_ch4/entity/BaseEntity.java
+++ spring_test/test_ch4/src/main/java/com/example/test_ch4/entity/BaseEntity.java
@@ -1,4 +1,17 @@
 package com.example.test_ch4.entity;
 
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+@Getter
+
 public class BaseEntity {
+
+    @CreatedDate
 }
