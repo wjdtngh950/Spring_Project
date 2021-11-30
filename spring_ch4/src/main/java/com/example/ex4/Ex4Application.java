84ca1bd103b258c9e36ba2a6200cbdd377757d5a
wjdtngh950 <wjdtngh950@naver.com>
2021-11-30 17:05:16 +0900

    Update Ex4Application.java

--- spring_ch4/src/main/java/com/example/ex4/Ex4Application.java
+++ spring_ch4/src/main/java/com/example/ex4/Ex4Application.java
@@ -2,8 +2,10 @@ package com.example.ex4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Ex4Application {
 
     public static void main(String[] args) {
