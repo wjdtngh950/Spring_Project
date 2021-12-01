d12730e3f0713f26fd0910546fa2d54ac27dc23a
wjdtngh950 <wjdtngh950@naver.com>
2021-12-01 22:51:17 +0900

    Update GuestbookRepository.java

--- spring_ch4/src/main/java/com/example/ex4/repository/GuestbookRepository.java
+++ spring_ch4/src/main/java/com/example/ex4/repository/GuestbookRepository.java
@@ -5,4 +5,3 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
-}
