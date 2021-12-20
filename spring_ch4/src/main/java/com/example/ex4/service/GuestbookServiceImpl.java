e1759e9651f4a8b30dba9f47193c05afab7c4f5c
wjdtngh950 <wjdtngh950@naver.com>
2021-12-20 21:50:44 +0900

    Update GuestbookServiceImpl.java

--- spring_ch4/src/main/java/com/example/ex4/service/GuestbookServiceImpl.java
+++ spring_ch4/src/main/java/com/example/ex4/service/GuestbookServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.ex4.service;
 
 import com.example.ex4.dto.GuestbookDTO;
+import com.example.ex4.entity.Guestbook;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,13 @@ public class GuestbookServiceImpl implements GuestbookService{
 
     @Override
     public Long register(GuestbookDTO dto) {
+
+        log.info("DTO--------------------------");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+
+        log.info(entity);
         return null;
     }
 }
