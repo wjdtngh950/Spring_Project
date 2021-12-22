15514943be33edbf01e234bc2ed9a4e429b885c5
wjdtngh950 <wjdtngh950@naver.com>
2021-12-22 23:54:15 +0900

    Update GuestbookServiceTests.java

--- spring_ch4/src/test/java/com/example/ex4/service/GuestbookServiceTests.java
+++ spring_ch4/src/test/java/com/example/ex4/service/GuestbookServiceTests.java
@@ -1,6 +1,9 @@
 package com.example.ex4.service;
 
 import com.example.ex4.dto.GuestbookDTO;
+import com.example.ex4.dto.PageRequestDTO;
+import com.example.ex4.dto.PageResultDTO;
+import com.example.ex4.entity.Guestbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,5 +24,15 @@ public class GuestbookServiceTests {
                 .build();
 
         System.out.println(service.register(guestbookDTO));
+    }
+
+    @Test
+    public void testList(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO=service.getList(pageRequestDTO);
+
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
     }
 }
