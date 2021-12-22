38e1e8633dd49caf0b2f5d3cb6d2574c307317dc
wjdtngh950 <wjdtngh950@naver.com>
2021-12-22 23:39:37 +0900

    Update GuestbookService.java

--- spring_ch4/src/main/java/com/example/ex4/service/GuestbookService.java
+++ spring_ch4/src/main/java/com/example/ex4/service/GuestbookService.java
@@ -1,10 +1,14 @@
 package com.example.ex4.service;
 
 import com.example.ex4.dto.GuestbookDTO;
+import com.example.ex4.dto.PageRequestDTO;
+import com.example.ex4.dto.PageResultDTO;
 import com.example.ex4.entity.Guestbook;
 
 public interface GuestbookService {
     Long register(GuestbookDTO dto);
+
+    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
 
     default Guestbook dtoToEntity(GuestbookDTO dto){
         Guestbook entity =Guestbook.builder()
@@ -14,5 +18,18 @@ public interface GuestbookService {
                 .writer(dto.getWriter())
                 .build();
         return entity;
+    }
+    default GuestbookDTO entityToDto(Guestbook entity) {
+
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+
+        return dto;
     }
 }
