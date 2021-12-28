1057d20e004ed808584e694caa62b1babc063662
wjdtngh950 <wjdtngh950@naver.com>
2021-12-28 17:59:20 +0900

    Update GuestbookController.java

--- spring_ch4/src/main/java/com/example/ex4/controller/GuestbookController.java
+++ spring_ch4/src/main/java/com/example/ex4/controller/GuestbookController.java
@@ -1,8 +1,11 @@
 package com.example.ex4.controller;
 
+import com.example.ex4.dto.PageRequestDTO;
+import com.example.ex4.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +14,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Log4j2
 @RequiredArgsConstructor
 public class GuestbookController {
+    private final GuestbookService service;
+
+    @GetMapping("/")
+    public String index(){
+
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("list............." + pageRequestDTO);
+
+        model
+    }
 
 
 }
