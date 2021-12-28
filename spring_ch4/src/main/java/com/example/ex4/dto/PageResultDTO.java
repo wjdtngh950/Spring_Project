43e904b8ebf12066887364641e6868cbd4878388
wjdtngh950 <wjdtngh950@naver.com>
2021-12-28 17:32:43 +0900

    Update PageResultDTO.java

--- spring_ch4/src/main/java/com/example/ex4/dto/PageResultDTO.java
+++ spring_ch4/src/main/java/com/example/ex4/dto/PageResultDTO.java
@@ -2,10 +2,13 @@ package com.example.ex4.dto;
 
 import lombok.Data;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
 
 @Data
 public class PageResultDTO<DTO, EN> {
@@ -27,9 +30,22 @@ public class PageResultDTO<DTO, EN> {
     //페이지 번호 목록
 
     private List<Integer> pageList;
-    
-    public PageResultDTO(Page<EN> result, Function<EN, DTO> fn){
+
+    public PageResultDTO(Page<EN> result, Function<EN, DTO> fn) {
 
         dtoList = result.stream().map(fn).collect(Collectors.toList());
+
+        totalPage = result.getTotalPages();
+
+        makePageList(result.getPageable());
+    }
+    private void makePageList(Pageable pageable){
+        this.page =pageable.getPageNumber()+1; //0부터 시작하므로 1을 추가
+        this.size = pageable.getPageSize();
+
+        //temp end page
+
+
+
     }
 }
