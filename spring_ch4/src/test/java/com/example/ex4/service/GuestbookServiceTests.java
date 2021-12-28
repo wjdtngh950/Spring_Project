3ddea98094fb96f4e335abac3af18669d0eb0122
wjdtngh950 <wjdtngh950@naver.com>
2021-12-28 16:40:56 +0900

    Update GuestbookServiceTests.java

--- spring_ch4/src/test/java/com/example/ex4/service/GuestbookServiceTests.java
+++ spring_ch4/src/test/java/com/example/ex4/service/GuestbookServiceTests.java
@@ -30,9 +30,11 @@ public class GuestbookServiceTests {
     public void testList(){
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
         PageResultDTO<GuestbookDTO, Guestbook> resultDTO=service.getList(pageRequestDTO);
+        
 
         for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
             System.out.println(guestbookDTO);
+
         }
     }
 }
