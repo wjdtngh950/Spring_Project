cd07c4b2cd1aeab35da79450587f24bc089821c5
wjdtngh950 <wjdtngh950@naver.com>
2021-12-28 17:57:05 +0900

    Update GuestbookController.java

--- spring_ch4/src/main/java/com/example/ex4/controller/GuestbookController.java
+++ spring_ch4/src/main/java/com/example/ex4/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.example.ex4.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
-    public String list(){
 
-        log.info("list..........");
-
-        return "/guestbook/list";
-    }
 }
