d1ed5526fbcd7049e4d2834ba0ecbcc776261450
wjdtngh950 <wjdtngh950@naver.com>
2021-12-28 17:35:32 +0900

    Update PageResultDTO.java

--- spring_ch4/src/main/java/com/example/ex4/dto/PageResultDTO.java
+++ spring_ch4/src/main/java/com/example/ex4/dto/PageResultDTO.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 @Data
@@ -44,8 +45,17 @@ public class PageResultDTO<DTO, EN> {
         this.size = pageable.getPageSize();
 
         //temp end page
+        int tempEnd = (int)(Math.ceil(page/10.0))* 10;
 
+        start = tempEnd -9;
 
+        prev = start >1;
 
+        end = totalPage > tempEnd ? tempEnd : totalPage;
+
+        next = totalPage > tempEnd;
+
+        pageList= IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+        
     }
 }
