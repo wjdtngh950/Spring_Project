30ccf35a531341341ccc3eb0c6583fce1d51a004
wjdtngh950 <wjdtngh950@naver.com>
2022-01-18 20:54:45 +0900

    Update PageResultDTO.java

--- spring_ch4/src/main/java/com/example/ex4/dto/PageResultDTO.java
+++ spring_ch4/src/main/java/com/example/ex4/dto/PageResultDTO.java
@@ -25,7 +25,11 @@ public class PageResultDTO<DTO, EN> {
     //목록 사이즈
     private int size;
 
-    //시작 페이지번호
+    //시작 페이지번호, 끝 페이지번호
+    private int start, end;
+
+    //이전, 다음
+    private boolean prev, next;
 
     //페이지 번호 목록
 
