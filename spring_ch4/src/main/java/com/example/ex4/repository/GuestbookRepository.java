0386bb73f995998c7d34eb68b1c39f9a6ea536c0
Jeong_suho <70904115+wjdtngh950@users.noreply.github.com>
2022-03-29 21:30:00 +0900

    delete

--- spring_ch4/src/main/java/com/example/ex4/repository/GuestbookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ex4.repository;
-
-import com.example.ex4.entity.Guestbook;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
-
-}
--- spring_test/src/main/java/com/example/test_ex2/repository/MemoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.test_ex2.repository;
-
-
-import com.example.test_ex2.entity.Memo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemoRepository extends JpaRepository<Memo, Long> {
-
-}
