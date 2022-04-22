d3d33a8aaf06c10eef9f7656cb3130d4699204ea
Jeong_suho <70904115+wjdtngh950@users.noreply.github.com>
2022-04-22 18:20:48 +0900

    Update MemoController.java

--- learningSpringBoot/suho-ex1/src/main/java/com/suho/suhoex1/controller/MemoController.java
+++ learningSpringBoot/suho-ex1/src/main/java/com/suho/suhoex1/controller/MemoController.java
@@ -1,7 +1,19 @@
 package com.suho.suhoex1.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1")
 public class MemoController {
+
+    @GetMapping("/test")
+    public void testController(){
+        log.info("요청 성공");
+  }
 }
