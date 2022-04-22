190ec9db98bd0910e9571edc8c5f5e7e6f4a6f8d
Jeong_suho <70904115+wjdtngh950@users.noreply.github.com>
2022-04-22 18:32:31 +0900

    Update MemoDto.java

--- learningSpringBoot/suho-ex1/src/main/java/com/suho/suhoex1/dto/MemoDto.java
+++ learningSpringBoot/suho-ex1/src/main/java/com/suho/suhoex1/dto/MemoDto.java
@@ -1,4 +1,19 @@
 package com.suho.suhoex1.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemoDto {
+    private String content;
+    private String writer;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
 }
